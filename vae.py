d40a8857ecbf85910c00918da11beac320fac6f4
simplepro <kdhsimplepro@gmail.com>
2024-08-14 23:37:57 +0900

    deleted files

--- vae.py
+++
@@ -1,3 +0,0 @@
-# 1. tripletnet으로 groundtruth distribution visualization 하기
-# 2. tripletnet으로 groundtruth의 code들을 추출하고, code를 reconstruction하는 VAE 학습하기
-# 3. 학습된 VAE로 충분히 많은 데이터를 generate하여 도출된 code distribution을 시각화하기
