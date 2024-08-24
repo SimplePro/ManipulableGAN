1a67ff1eed38f1b495b9ab67518313039df08974
simplepro <kdhsimplepro@gmail.com>
2024-08-25 00:41:00 +0900

    modified

--- real_nvp.py
+++ real_nvp.py
@@ -4,35 +4,46 @@ from torch import distributions
 
 import numpy as np
 
-device = "cpu"
+from time import time
+
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+
+import os
+
+import matplotlib.pyplot as plt
 
 class RealNVP(nn.Module):
-    def __init__(self):
+    def __init__(self, dim=16):
         super(RealNVP, self).__init__()
 
         nets = lambda: nn.Sequential(
-            nn.Linear(16, 128),
+            nn.Linear(dim, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 128),
             nn.LeakyReLU(),
-            nn.Linear(128, 16),
+            nn.Linear(128, dim),
             nn.Tanh()
         )
 
         nett = lambda: nn.Sequential(
-            nn.Linear(16, 128),
+            nn.Linear(dim, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 128),
             nn.LeakyReLU(),
             nn.Linear(128, 128),
             nn.LeakyReLU(),
-            nn.Linear(128, 16)
+            nn.Linear(128, dim)
         )
 
-        self.masks = torch.from_numpy(np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]] * 3).astype(np.float32))
-        self.prior = distributions.MultivariateNormal(torch.zeros(16), torch.eye(16))
+        self.masks = torch.from_numpy(np.array([[0 if i < dim // 2 else 1 for i in range(dim)], [1 if i < dim // 2 else 0 for i in range(dim)]] * 3).astype(np.float32))
+        # print(self.masks)
+        # self.masks = torch.from_numpy(np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]] * 3).astype(np.float32))
+        self.prior = distributions.MultivariateNormal(torch.zeros(dim), torch.eye(dim))
         
         self.t = torch.nn.ModuleList([nett().to(device) for _ in range(len(self.masks))])
         self.s = torch.nn.ModuleList([nets().to(device) for _ in range(len(self.masks))])
@@ -76,7 +87,7 @@ class RealNVP(nn.Module):
 
 
 def train_real_nvp(data, iters):
-    flow = RealNVP()
+    flow = RealNVP(dim=data[0].shape[1])
 
     optimizer = torch.optim.Adam([p for p in flow.parameters() if p.requires_grad==True], lr=1e-4)
 
@@ -94,3 +105,85 @@ def train_real_nvp(data, iters):
             print("iter %s" % t, "loss = %.3f" % loss)
             
     return flow
+
+
+if __name__ == '__main__':
+
+    device = "cpu"
+
+    dataset = "mnist"
+    expr_name = "2d_original_perceptual_wgan-gp"
+
+    if expr_name == "2d_original_perceptual_wgan-gp":
+        from original_experiments import TripletNet, ManipulableAutoEncoder, Generator
+
+    else:
+        from gan import TripletNet, ManipulableAutoEncoder, Generator
+
+    root_dir = os.path.join(f"{dataset}_results", expr_name)
+
+    tripletnet = TripletNet().to(device)
+    tripletnet.load_state_dict(torch.load(os.path.join(root_dir, "mnist_tripletnet_state_dict.pth")))
+    tripletnet.eval()
+
+    if expr_name != "2d_original_perceptual_wgan-gp":
+        manipulable_ae = ManipulableAutoEncoder().to(device)
+        manipulable_ae.load_state_dict(torch.load(os.path.join(root_dir, "mnist_manipulable_ae_state_dict.pth")))
+        manipulable_ae.eval()
+
+    gen = Generator().to(device)
+    gen.load_state_dict(torch.load(os.path.join(root_dir, "mnist_gen_state_dict.pth")))
+    gen.eval()
+    
+    trainset = MNIST(
+        root="./MNIST/images/",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor()
+    )
+
+    validset = MNIST(
+        root="./MNIST/images/",
+        train=False,
+        download=True,
+        transform=transforms.ToTensor()
+    )
+    
+    codes = []
+
+    for x, y in trainset:
+        pred = tripletnet(x.to(device).unsqueeze(0))
+        codes.append(pred.detach())
+        
+    start_time = time()
+
+    flow = train_real_nvp(codes, iters=2000)
+    torch.save(flow.state_dict(), os.path.join(root_dir, "real_nvp_state_dict.pth"))
+    samples = flow.sample(100000)
+
+    if expr_name == "2d_original_perceptual_wgan-gp":
+        codes_2d = [code.reshape(-1) for code in codes]
+
+    else:
+
+        codes_2d = []
+
+        for sample in samples:
+            codes_2d.append(manipulable_ae.encode(sample.unsqueeze(0)).reshape(-1).detach())
+
+    elapsed_time = int(time() - start_time)
+
+    print(f"elapsed_time: {elapsed_time//3600}h {elapsed_time%3600//60}m {elapsed_time%60}s")
+
+    plt.figure(figsize=(6, 5))
+    plt.scatter([codes_2d[i][0].item() for i in range(len(codes_2d))], [codes_2d[i][1].item() for i in range(len(codes_2d))], s=0.1, alpha=0.05)
+    plt.xlim((-8, 6))
+    plt.ylim((-8, 9))
+    plt.savefig(os.path.join(root_dir, "real_nvp_2d_visualization.png"))
+    # plt.show()
+
+    pred = gen(samples[:100])
+
+    grid = make_grid(pred, nrow=10, normalize=True)
+    img = TF.resize(TF.to_pil_image(grid), (512, 512))
+    img.save(os.path.join(root_dir, "samples.png"))
